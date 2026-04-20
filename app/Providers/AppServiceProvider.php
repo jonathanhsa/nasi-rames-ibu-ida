64dc172cc1a19758e6866854f23548789176849a
jonathanhsa <saragihjonathan80@gmail.com>
2026-04-20 13:29:26 +0700

    Force HTTPS to fix 419 error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (config('app.env') === 'production' || config('app.url') !== 'http://localhost') {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
     }
 }
